308e202370c109825257d56e06fc32df22a7ae26
Arthur Upfield <arthur@salmonline.us>
2025-09-01 18:30:09 -0400

    Created BasePage POM functionality, starting LoginPage

---
+++ demo/src/main/java/com/saucedemo/pages/BasePage.java
@@ -0,0 +1,33 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+
+    public static WebDriver driver;
+
+    public void setDriver(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    // Eliminates the need for us to type `driver.findElement()` each time - we can just use `find()`
+    // Protected, as we only want to reuse this method in our base page & page object classes
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    // Passing in a String for the data our test script will set into an element
+    // By is used for locating an element
+    protected void set(By locator, String text) {
+        // If there is data in the element, clear it, and send the text
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    // Clicks any element on the webpage
+    protected void click(By locator) {
+        find(locator).click();
+    }
+}
