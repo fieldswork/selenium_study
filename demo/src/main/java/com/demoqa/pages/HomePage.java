b85288d40315f866064265ee47037ef276b63036
Arthur Upfield <arthur@salmonline.us>
2025-09-02 19:38:56 -0400

    Adding pages for parts 3, 4

---
+++ demo/src/main/java/com/demoqa/pages/HomePage.java
@@ -0,0 +1,10 @@
+package com.demoqa.pages;
+
+import com.saucedemo.pages.BasePage;
+
+public class HomePage extends BasePage {
+    // Note that we're extending BasePage here, which belongs to saucedemo - 
+    // This is fine, since BasePage has utility methods that are reusable here!
+
+    
+}
